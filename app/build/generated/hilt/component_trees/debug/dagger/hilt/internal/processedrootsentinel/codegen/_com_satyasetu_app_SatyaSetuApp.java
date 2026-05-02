2d489bfa097733a11374941759e3f6e8596f47ce
Hrishikesh Giri <goswamirishi83@gmail.com>
2026-05-03 00:56:28 +0530

    Add comprehensive .gitignore and remove build artifacts from tracking

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_satyasetu_app_SatyaSetuApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.satyasetu.app.SatyaSetuApp"
-)
-public final class _com_satyasetu_app_SatyaSetuApp {
-}
